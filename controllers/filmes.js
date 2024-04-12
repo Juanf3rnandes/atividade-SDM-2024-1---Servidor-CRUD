8449c7b762922b2cea48387ac858fc66b366b63f
juan_f3rnandes <juanpereira@hubert.com.br>
2024-04-12 10:46:27 -0300

    update

---
+++ controllers/filmes.js
@@ -0,0 +1,33 @@
+import sgbd from "../sgbd.js";
+
+const route = "/filmes";
+const entity = "filmes";
+
+function filmes(app) {
+  app.get(route, function (req, res) {
+    res.json(sgbd.db[entity]);
+  });
+
+  app.get(route + "/:id", function (req, res) {
+    res.json(sgbd.db[entity][req.params.id]);
+  });
+
+  app.post(route, function (req, res) {
+    sgbd.db[entity][req.body.filme] = req.body.filme;
+    sgbd.write();
+    res.json(sgbd.db[entity][req.body.filme]);
+  });
+
+  app.put(route + "/:id", function (req, res) {
+    sgbd.db[entity][req.params.id] = req.body.filme;
+    res.json(sgbd.db[req.body.filme]);
+  });
+
+  app.delete(route + "/:id", function (req, res) {
+    delete sgbd.db[entity][req.params.id];
+    sgbd.write();
+    res.json({});
+  });
+}
+
+export default filmes;
